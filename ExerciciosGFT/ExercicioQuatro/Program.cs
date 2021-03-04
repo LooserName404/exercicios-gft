f5fd848fe08a0a70ef969d4e9754d3323ade79d1
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2021-03-04 16:05:49 -0300

    feat: resolver quarto exercício

---
+++ ExerciciosGFT/ExercicioQuatro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ExercicioQuatro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, bem-vindo!");
+
+            int repeticoes = 0;
+            bool temErros = false;
+            do
+            {
+                temErros = false;
+                Console.Write("Digite um número pra gente tocar a música: ");
+                if (!int.TryParse(Console.ReadLine(), out repeticoes))
+                {
+                    Console.WriteLine("Digite um número válido!");
+                    temErros = true;
+                }
+            } while (temErros);
+
+            if (repeticoes == 1)
+            {
+                Console.WriteLine("Um elefante incomoda muita gente!");
+            }
+            else
+            {
+                string incomodam = "";
+                for (int i = 1; i <= repeticoes; i++)
+                {
+                    incomodam += "Incomodam ";
+                }
+                Console.WriteLine("{0} elefantes {1}muito mais!", repeticoes, incomodam);
+            }
+        }
+    }
+}
